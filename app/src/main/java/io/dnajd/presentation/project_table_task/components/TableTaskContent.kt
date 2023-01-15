1d4724ead6677fec1d06b35d4afb194600ac93c4
d najd <dimitar.hristijan.najdovski@gmail.com>
2023-01-15 20:19:28 +0100

    minor refactoring

--- app/src/main/java/io/dnajd/presentation/project_table_task/components/TableTaskContent.kt
+++ app/src/main/java/io/dnajd/presentation/project_table_task/components/TableTaskContent.kt
@@ -21,16 +21,8 @@ fun TableTaskContent(
             .fillMaxSize()
             .padding(contentPadding),
     ) {
-        val task = state.task
-        Text(text = "Task id: ${task.id}")
-        Text(text = "Table id: ${task.tableId}")
-        Text(text = "Title: ${task.title}")
-        Text(text = "Description: ${task.description}")
-        Text(text = "Position: ${task.position}")
-        Text(text = "Comments count: ${task.comments.size}")
-        Text(text = "Labels count: ${task.labels.size}")
-        Text(text = "Assigned count: ${task.assigned.size}")
-        Text(text = "Created At: ${task.createdAt}")
-        Text(text = "Updated At: ${task.updatedAt}")
+
+
+        // TableTaskBasicInfo(state)
     }
 }
