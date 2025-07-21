3d55b416a36a53bf1d6b376b0e0b0bd04cd3c877
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-07-21 13:46:13 +0200

    feat: minor rework of RepositoryBase.kt

--- app/src/main/java/io/dnajd/data/utils/RepositoryBase.kt
+++ app/src/main/java/io/dnajd/data/utils/RepositoryBase.kt
@@ -7,32 +7,38 @@ import androidx.compose.runtime.remember
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.util.Date
 
 /**
  * Sets are recommended here since ordering of items should be irrelevant and sets not allowing
  * duplicates
+ *
+ * [V] is instance of [Date] because in most cases repositories will begin using [Date], and maybe
+ * begin using another value later down the line. In this case we will have default implementation
+ * for base [Date] and we can check whether [V] is [Date] itself and if it is use the original
+ * implementation till we transition fully (if at all).
  */
-open class RepositoryBase<T1, T2, S>(initialState: S) where S : RepositoryBase.State<T1, T2> {
+open class RepositoryBase<K, V, S>(initialState: S) where S : RepositoryBase.State<K, V>, V : Date {
 	protected val mutableState: MutableStateFlow<S> = MutableStateFlow(initialState)
 	protected val state: StateFlow<S> = mutableState.asStateFlow()
 
 	@Composable
-	open fun dataKeysCollected(): Set<T1> {
+	open fun dataKeysCollected(): Set<K> {
 		val stateCollected by state.collectAsState()
 		return remember(stateCollected) {
 			stateCollected.data.keys
 		}
 	}
 
-	open fun dataKeys(): Set<T1> = state.value.data.keys
+	open fun dataKeys(): Set<K> = state.value.data.keys
 
-	open fun data(): Map<T1, T2> = state.value.data
+	open fun data(): Map<K, V> = state.value.data
 
-	open class State<T1, T2>(
+	open class State<K, V>(
 		/**
 		 * The key is the thing being stored and the value is for keeping track of
 		 * the last time it was updated
 		 */
-		open val data: Map<T1, T2> = emptyMap(),
-	)
+		open val data: Map<K, V> = emptyMap(),
+	) where V : Date
 }
