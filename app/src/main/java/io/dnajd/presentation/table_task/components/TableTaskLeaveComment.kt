e967ed198522ab64bd1c54eaffe227a6b1e1b43c
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-08-03 22:19:42 +0200

    feat!: added send comment button for commenting

--- app/src/main/java/io/dnajd/presentation/table_task/components/TableTaskLeaveComment.kt
+++ app/src/main/java/io/dnajd/presentation/table_task/components/TableTaskLeaveComment.kt
@@ -1,42 +1,104 @@
 package io.dnajd.presentation.table_task.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import io.dnajd.bugtracker.R
+import io.dnajd.presentation.util.rememberKeyboardState
 
 @Composable
 fun TableTaskLeaveComment() {
-	LocalDensity.current
 	Column(
 		modifier = Modifier.fillMaxWidth(),
 		verticalArrangement = Arrangement.Bottom,
 	) {
 		HorizontalDivider()
 
-		TextField(
-			modifier = Modifier.fillMaxWidth(),
-			value = "Test",
-			onValueChange = { },
-			placeholder = {
-				Text(stringResource(R.string.info_comment_first))
-			},
-			colors = TextFieldDefaults.colors(
-				focusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
-				unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
-				focusedIndicatorColor = Color.Transparent,
-				unfocusedIndicatorColor = Color.Transparent,
-			),
-		)
+		Row(
+			modifier = Modifier
+				.fillMaxWidth()
+				.background(MaterialTheme.colorScheme.surfaceContainer),
+		) {
+			val isKeyboardOpen by rememberKeyboardState()
+			val focusRequester = remember { FocusRequester() }
+			val focusManager = LocalFocusManager.current
+			var isFocused by remember { mutableStateOf(false) }
+
+			LaunchedEffect(isKeyboardOpen, isFocused) {
+				if (!isKeyboardOpen && isFocused) {
+					focusManager.clearFocus(true)
+				}
+			}
+
+			TextField(
+				modifier = Modifier
+					.weight(1f)
+					.focusRequester(focusRequester)
+					.onFocusChanged { focusState ->
+						isFocused = focusState.isFocused
+					},
+				value = "Test",
+				onValueChange = { },
+				placeholder = {
+					Text(stringResource(R.string.info_comment_first))
+				},
+				colors = TextFieldDefaults.colors(
+					focusedContainerColor = Color.Transparent,
+					unfocusedContainerColor = Color.Transparent,
+					focusedIndicatorColor = Color.Transparent,
+					unfocusedIndicatorColor = Color.Transparent,
+				),
+				keyboardOptions = KeyboardOptions.Default.copy(
+					imeAction = ImeAction.Send,
+				),
+				keyboardActions = KeyboardActions(
+					onSend = {
+
+					},
+				),
+			)
+
+			IconButton(
+				modifier = Modifier
+					.padding(end = 8.dp),
+				onClick = {
+
+				},
+			) {
+				Icon(
+					imageVector = Icons.AutoMirrored.Filled.Send,
+					contentDescription = null,
+				)
+			}
+		}
 	}
 }
