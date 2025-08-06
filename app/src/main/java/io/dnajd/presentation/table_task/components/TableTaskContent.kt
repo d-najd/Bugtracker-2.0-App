ab795f4f0a262598413f0bbfaf3b608b948c11fe
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-08-06 22:22:41 +0200

    fix: disabled checkbox interaction in tabletask

--- app/src/main/java/io/dnajd/presentation/table_task/components/TableTaskContent.kt
+++ app/src/main/java/io/dnajd/presentation/table_task/components/TableTaskContent.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -67,6 +68,12 @@ fun TableTaskContent(
 				Checkbox(
 					modifier = Modifier.size(16.dp),
 					checked = true,
+					enabled = false,
+					colors = CheckboxDefaults.colors()
+						.copy(
+							disabledCheckedBoxColor = CheckboxDefaults.colors().checkedBoxColor,
+							disabledBorderColor = CheckboxDefaults.colors().checkedBorderColor,
+						),
 					onCheckedChange = { },
 				)
 				Text(
